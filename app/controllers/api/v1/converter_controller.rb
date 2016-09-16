552e8852ce9175031366d0142a20df6d8979df7a
sandeep <sandeep@systematixtechnocrates.com>
2016-09-16 12:48:03 +0530

    added html format

--- app/controllers/api/v1/converter_controller.rb
+++ app/controllers/api/v1/converter_controller.rb
@@ -9,10 +9,12 @@ class Api::V1::ConverterController < ApplicationController
     OEmbed::Providers.register(OEmbed::Providers::Youtube, OEmbed::Providers::Instagram)
     resource = OEmbed::Providers.get(params[:url])
     respond_to do |format|
+      format.html { render json: { error: false, html: resource.html } }
       format.json { render json: { error: false, html: resource.html } }
     end
   rescue
     respond_to do |format|
+      format.html { render json: { error: true, message: 'Something wrong with URL' } }
       format.json { render json: { error: true, message: 'Something wrong with URL' } }
     end
   end
